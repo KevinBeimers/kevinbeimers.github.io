1833c0b2659ad9e6ac4b7d960e30d447603cbb5c
Kevin <74053135+KevinBeimers@users.noreply.github.com>
2025-11-01 12:04:21 +0000

    Add files via upload

---
+++ formtest.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Untitled Document</title>
+<link href="css/style.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+	<?php
+	$txt = "PHP";
+	echo "I love $txt!";
+	?>
+	
+</body>
+</html>
